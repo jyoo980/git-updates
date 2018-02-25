b83a522100fd27bc6d6f6289caf3833d331d6713
James Yoo <jyoo980@gmail.com>
2018-02-25 14:57:58 -0800

    Stared design of user landing page UI

---
+++ GitUp/GHUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GHUserTableViewCell.swift
+//  GitUp
+//
+//  Created by James Yoo on 2018-02-25.
+//  Copyright © 2018 James Yoo. All rights reserved.
+//
+
+import UIKit
+
+class GHUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
