684b0e2936224fe1e4da02971a6512c1557735f3
James Yoo <jyoo980@gmail.com>
2018-02-25 20:10:57 -0800

    Added AddUserController.swift

---
+++ GitUp/Controller/AddUserController.swift
@@ -0,0 +1,24 @@
+//
+//  AddUserController.swift
+//  GitUp
+//
+//  Created by James Yoo on 2018-02-25.
+//  Copyright © 2018 James Yoo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddUserController : UIViewController {
+    
+    @IBOutlet weak var userSearchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        // TODO
+    }
+    
+    
+    
+}
+
+
