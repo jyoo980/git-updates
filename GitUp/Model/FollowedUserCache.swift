5759023d64612ce01f96fe3bb723bc4ab7623d06
James Yoo <jyoo980@gmail.com>
2018-03-03 14:24:05 -0800

    Added FollowedUserCache

---
+++ GitUp/Model/FollowedUserCache.swift
@@ -0,0 +1,13 @@
+//
+//  FollowedUserCache.swift
+//  GitUp
+//
+//  Created by James Yoo on 2018-03-03.
+//  Copyright © 2018 James Yoo. All rights reserved.
+//
+
+import Foundation
+
+class FollowedUserCache {
+    
+}
