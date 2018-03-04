d9588340594920f34f687864c93979b1f09385d4
James Yoo <jyoo980@gmail.com>
2018-03-03 17:13:12 -0800

    Added class MainViewController.swift

---
+++ GitUp/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  GitUp
+//
+//  Created by James Yoo on 2018-03-03.
+//  Copyright © 2018 James Yoo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO
+        return -1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO
+        return UITableViewCell()
+    }
+
+}
